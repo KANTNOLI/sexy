7451895f41540f3475f049f394721750d15d9015
KANTNOLI <zahargysev02@gmail.com>
2025-08-26 21:37:08 +0300

    1

---
+++ src/app/IloveYOUsasha%3C3/page.tsx
@@ -0,0 +1,40 @@
+"use client"
+
+import { useEffect, useState } from "react";
+import style from "./page.module.scss"
+
+export default function Home() {
+  const [Page, setPage] = useState<number>(1)
+  const size = 70;
+
+  useEffect(() => {
+    console.log(Page);
+    
+  }, [Page])
+
+  const handlerNext = () => {
+    if (Page < size) {
+      setPage(before => before + 1)
+    } else {
+      setPage(1)
+    }
+  }
+
+    const handlerBef = () => {
+    if (Page > 1) {
+      setPage(before => before - 1)
+    } else {
+      setPage(size)
+    }
+  }
+
+  return (
+    <div className={style.main}>
+      <div className={style.img}>
+        <img src={`./photo_${Page}_2025-08-26_19-51-22.jpg`} alt="" />
+        <button onClick={() => handlerBef()} className={style.b1}>Назад</button>
+        <button onClick={() => handlerNext()} className={style.b2}>Вперед</button>
+      </div>
+    </div>
+  );
+}
