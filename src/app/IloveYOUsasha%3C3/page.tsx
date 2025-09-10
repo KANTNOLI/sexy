ba928c6b65855d70273c2730906bbe9aac0a0f1f
KANTNOLI <zahargysev02@gmail.com>
2025-09-10 22:01:44 +0300

    1

--- src/app/IloveYOUsasha%3C3/page.tsx
+++ src/app/IloveYOUsasha%3C3/page.tsx
@@ -7,11 +7,11 @@ import style from "./page.module.scss"
 
 export default function Home() {
   const [Page, setPage] = useState<number>(1)
-  const size = 70;
+  const size = 133;
 
   useEffect(() => {
     console.log(Page);
-    
+
   }, [Page])
 
   const handlerNext = () => {
@@ -22,7 +22,7 @@ export default function Home() {
     }
   }
 
-    const handlerBef = () => {
+  const handlerBef = () => {
     if (Page > 1) {
       setPage(before => before - 1)
     } else {
@@ -33,7 +33,7 @@ export default function Home() {
   return (
     <div className={style.main}>
       <div className={style.img}>
-        <img src={`./photo_${Page}_2025-08-26_19-51-22.jpg`} alt="" />
+        <img src={`./photo_${Page}_2025-09-10_21-49-50.jpg`} alt="" />
         <button onClick={() => handlerBef()} className={style.b1}>Назад</button>
         <button onClick={() => handlerNext()} className={style.b2}>Вперед</button>
       </div>
